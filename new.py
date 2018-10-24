47ddc3219a599c3ebbba84153f06bfdc212e9abb
nctl <44381400+nctl@users.noreply.github.com>
2018-10-24 14:38:45 -0700

    learned a few things
    1234

---
+++ new.py
@@ -0,0 +1,46 @@
+print("Hello World!")
+
+# Apparently I'm going to slow, so i will speed up
+# This is a comment
+# This has no effect on the code
+# This is used for a variety of things, such as
+# 1. Making personal notes about my code
+# 2. Commenting out code that does not work
+
+print("Notice what is happening here.")
+print()  # This creates a new line
+print()  # Do you notice the underline here?
+
+# Hover on top of it to see what is wrong.
+
+# Math
+print(5 + 3)
+print(5 - 3)
+print(4 * 5)
+print(6 / 5)
+
+# Intermediate Math
+print("Figure this out.")
+print(6 // 4)
+print(12 // 3)
+# Using "//" in math will ONLY give an integer, no decimals
+print()
+print(6 % 4)
+print(5 % 3)
+print(9 % 4)
+print()
+# Defining Variables
+car_name = "Wiebe Mobile"
+car_type = "Tesla"
+car_cylinders = 16
+car_miles_per_gallon = 2
+
+print("I have a car called %s. It's pretty nice." % car_name)
+print("It has %d cylinders, but gets %f MPG" % (car_cylinders, car_miles_per_gallon))
+
+# Taking Input
+name = input("What is your name?")
+print("Hello %s" % name)
+
+age = input("How old are you?")
+print ("%s" % age)
