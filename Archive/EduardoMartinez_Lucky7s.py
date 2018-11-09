dd49e6b9a220cfe2e0689b66fea3d1a86addee51
nctl <44381400+nctl@users.noreply.github.com>
2018-11-09 14:43:08 -0800

    Lucky sevens complete
    made November 9, 2018

---
+++ Archive/EduardoMartinez_Lucky7s.py
@@ -0,0 +1,19 @@
+import random
+active = True
+money = 15
+rounds_played = 0
+while active and money > 0:
+    rounds_played = rounds_played + 1
+    dice_1 = random.randint(1, 6)
+    dice_2 = random.randint(1, 6)
+    print(dice_1 + dice_2)
+    if dice_1 + dice_2 == 7:
+        money = money + 4
+        print("Lucky! You won $5! You now have %d" % money)
+    else:
+        money = money - 1
+        print("You have lost the $1 bet and now have %d" % money)
+
+if money == 0:
+    print("Game over! You played for %d rounds before running out of money." % rounds_played)
+    active = False
