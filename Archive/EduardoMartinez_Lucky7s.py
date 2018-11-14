d4a66330eaa4108bbbf06ae9748de365e64ad265
nctl <44381400+nctl@users.noreply.github.com>
2018-11-14 14:08:58 -0800

    Backup (olde)
    made November 14, 2018

--- Archive/EduardoMartinez_Lucky7s.py
+++ Archive/EduardoMartinez_Lucky7s.py
@@ -8,11 +8,11 @@ while active and money > 0:
     dice_2 = random.randint(1, 6)
     print(dice_1 + dice_2)
     if dice_1 + dice_2 == 7:
-        money = money + 5.1
-        print("Lucky! You won $5 and now have $%d" % money)
+        money = money + 4
+        print("Lucky! You won $5 and now have $%d." % money)
     else:
         money = money - 1
-        print("You have lost the $1 bet and now have $%d left" % money)
+        print("You have lost the $1 bet and now have $%d left." % money)
 
 if money <= 0:
     print("Game over! You played for %d rounds before running out of money." % rounds_played)
