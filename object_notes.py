74a34bd5a3ab95bbd173b7b556ee0cdb0f98b3e7
Eduardo Martinez Pineda 8936 <nctl@fresnou.org>
2019-02-13 14:36:30 -0800

    Object Thingy
    Made 2/13/19

--- object_notes.py
+++ object_notes.py
@@ -1,30 +1,63 @@
+import special_random
+
+
 class Laptop(object):
-    def __init__(self, screen_resolution, storage_available):
+    def __init__(self, screen_resolution, storage_available=1000, color="Cobalt"):
         # Everything in this list should be relevant
         self.processor = "Intel i5"
         self.screen_resolution = screen_resolution
         self.battery_remaining = 100
-        self.storage_available = storage_available
-        self.color = "Black and Silver"
+        self.storage_available = storage_available  # Measured in GB
+        self.color = color
         self.os = "Windows 10"
+        self.functioning = True
 
     def charge(self, time):
-        self.battery_remaining += time
+        if self.functioning:
+            self.battery_remaining += time  # This restores the battery over time.
 
-        if self.battery_remaining >= 100:
-            self.battery_remaining = 100
+            if self.battery_remaining >= 100:
+                self.battery_remaining = 100
 
-        # Full charge
-        if self.battery_remaining == 100:
-            print("Your PC is fully charged.")
-            return
+            # Full charge
+            if self.battery_remaining == 100:
+                print("Your PC is fully charged.")
+                return
 
-        # Low charge
-        if self.battery_remaining < 100:
-            print("%d power left" % self.battery_remaining)  # needs changing
+            # Low charge
+            if self.battery_remaining < 100:
+                print("%d power left" % self.battery_remaining)  # needs changing
 
-        # No charge
+            # No charge
+            else:
+                print("%d power left" % self.battery_remaining)
+
         else:
-            print("%d power left" % self.battery_remaining)
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+            print("Your computer is DEAD. At least you tried.")
 
+    def smash(self):
+        self.functioning = False
+    print("I took the laptop...")
+    print("AND THREW IT ON THE GROUND!!!")
+
+    def use(self, time):
+        self.battery_remaining -= time
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+my_computer = Laptop("1920x1080", 10000, "Black")
+your_computer = Laptop("240p", 16, "Orange")
+default_computer = Laptop("1920x1080")
+
+
+my_computer.use(60)
+my_computer.charge(20)
+my_computer.charge(100)
+my_computer.smash()
+my_computer.charge(20)
+
+default_computer.charge(20)
+
+# CREATE AN OBJECT CLASS WITH 2 OR MORE OBJECTS.
+# THEN GIVE EACH OBJECT 3 OR MORE METHODS
---
+++ special_random.py
@@ -0,0 +1,7 @@
+import random
+
+
+class Random:
+    @staticmethod
+    def special_random():
+        return random.randint(1, 100)
