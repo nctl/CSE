69bdf9a77353da33710d5adca85c530bc85c5aa4
nctl <44381400+nctl@users.noreply.github.com>
2019-05-02 14:25:29 -0700

    Finished Validation
    Made 5/2/19

--- EduardoMartinez_Validator.py
+++ EduardoMartinez_Validator.py
@@ -1,24 +1,17 @@
 import csv
 
-# with open("Book1.csv", 'r') as old_csv:
-#     with open("CreditCardNumlist.csv", 'w', newline='') as new_csv:
-#         reader = csv.reader(old_csv)
-#         writer = csv.writer(new_csv)
-#         print("Writing File...")
-#
-#         for row in reader:
-#             old_number = row[0]
-#             if validate(old_number):
-#                 writer.writerow(row)
-#         print("Done")
-
 
 def validate(num: str):
     all_digits = list(num)
     last_digit = all_digits[15]
     all_digits.pop(15)
     all_digits = reverse_it(all_digits)
-    print(all_digits)
+    multiply_odd_digits(all_digits)
+    total_sum = sum(num)
+    if total_sum % 10 == int(last_digit):
+        return True
+    else:
+        return False
 
 
 def reverse_it(all_digits: list):
@@ -26,12 +19,23 @@ def reverse_it(all_digits: list):
     return all_digits
 
 
-def multiply_odd_digits(num: str):
+def multiply_odd_digits(num: list):
     for index in range(len(num)):
-        integer_version = int(num[index])
+        num[index] = int(num[index])
+        if index % 2 == 0:
+            num[index] *= 2
+            if num[index] > 9:
+                num[index] -= 9
 
 
-def subtract_large_numbers(num: str):
-    for i in all_digits:
-        if i > 9:
-            i -= 9
+with open("Book1.csv", 'r') as old_csv:
+    with open("CreditCardNumlist.csv", 'w', newline='') as new_csv:
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        print("Writing File...")
+
+        for row in reader:
+            old_number = row[0]
+            if validate(old_number):
+                writer.writerow(row)
+        print("Done")
