12e5210829f65a57689ab66997d823a9db98e643
nctl <44381400+nctl@users.noreply.github.com>
2019-05-03 14:48:28 -0700

    Even More Valid
    Made 5/3/2019

--- EduardoMartinez_Validator.py
+++ EduardoMartinez_Validator.py
@@ -7,11 +7,8 @@ def validate(num: str):
     all_digits.pop(15)
     all_digits = reverse_it(all_digits)
     multiply_odd_digits(all_digits)
-    total_sum = sum(num)
-    if total_sum % 10 == int(last_digit):
-        return True
-    else:
-        return False
+    total_sum = sum(all_digits)
+    mod_10(all_digits)  # Bad
 
 
 def reverse_it(all_digits: list):
@@ -28,14 +25,20 @@ def multiply_odd_digits(num: list):
                 num[index] -= 9
 
 
+def mod_10(total_sum, last_digit):  # May need repair
+    if int(total_sum) % 10 == int(last_digit):
+        return True
+    else:
+        return False
+
+
 with open("Book1.csv", 'r') as old_csv:
-    with open("CreditCardNumlist.csv", 'w', newline='') as new_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
         print("Writing File...")
-
         for row in reader:
-            old_number = row[0]
-            if validate(old_number):
+            num = row[0]
+            if validate(num):  # Bad
                 writer.writerow(row)
         print("Done")
