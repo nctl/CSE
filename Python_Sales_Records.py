9175b516523a530b7671daa246ea5ed9652e32e0
nctl <44381400+nctl@users.noreply.github.com>
2019-05-14 14:38:09 -0700

    Extension 2 1/2
    Made 5/14/2019

--- Python_Sales_Records.py
+++ Python_Sales_Records.py
@@ -1,7 +1,5 @@
 import csv
-
-# What is the product with the highest profit margin per unit?
-
+# DO LINE 178!!
 with open("Sales Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
     fruits_profit_list = []
@@ -24,57 +22,85 @@ with open("Sales Records.csv", 'r') as old_csv:
     asia_profit_list = []
     c_america_and_caribbean_profit_list = []
     n_america_profit_list = []
+
+    fruits_units = []
+    clothes_units = []
+    meat_units = []
+    beverages_units = []
+    office_supplies_units = []
+    cosmetics_units = []
+    snacks_units = []
+    personal_care_units = []
+    household_units = []
+    baby_food_units= []
+    vegetables_units = []
+    cereal_units = []
+
     for row in reader:
         region = row[0]
         category = row[2]
+        units_sold = row[8]
         profit = row[13]
+        #  no more rows are needed here
         if category == "Fruits":
             #  print(region, category, profit)
             fruits_profit_list.append(float(profit))
+            fruits_units.append(int(profit))
 
         if category == "Clothes":
             #  print(region, category, profit)
             clothes_profit_list.append(float(profit))
+            clothes_units.append(int(units_sold))
 
         if category == "Meat":
             #  print(region, category, profit)
             meat_profit_list.append(float(profit))
+            meat_units.append(int(units_sold))
 
         if category == "Beverages":
             #  print(region, category, profit)
             beverages_profit_list.append(float(profit))
+            beverages_units.append(int(units_sold))
 
         if category == "Office Supplies":
             #  print(region, category, profit)
             office_supplies_profit_list.append(float(profit))
+            office_supplies_units.append(int(units_sold))
 
         if category == "Cosmetics":
             #  print(region, category, profit)
             cosmetics_profit_list.append(float(profit))
+            cosmetics_units.append(int(units_sold))
 
         if category == "Snacks":
             #  print(region, category, profit)
             snacks_profit_list.append(float(profit))
+            snacks_units.append(int(units_sold))
 
         if category == "Personal Care":
             #  print(region, category, profit)
             personal_care_profit_list.append(float(profit))
+            personal_care_units.append(int(units_sold))
 
         if category == "Household":
             #  print(region, category, profit)
             household_profit_list.append(float(profit))
+            household_units.append(int(units_sold))
 
         if category == "Baby Food":
             #  print(region, category, profit)
             baby_food_profit_list.append(float(profit))
+            baby_food_units.append(int(units_sold))
 
         if category == "Vegetables":
             #  print(region, category, profit)
             vegetables_profit_list.append(float(profit))
+            vegetables_units.append(int(units_sold))
 
         if category == "Cereal":
             #  print(region, category, profit)
             cereal_profit_list.append(float(profit))
+            cereal_units.append(int(units_sold))
 
         if region == "Sub-Saharan Africa":
             sub_saharan_africa_profit_list.append(float(profit))
@@ -118,6 +144,38 @@ asia_sum = sum(asia_profit_list)
 c_america_and_caribbean_sum = sum(c_america_and_caribbean_profit_list)
 n_america_sum = sum(n_america_profit_list)
 
+fruits_total_units = sum(fruits_units)
+clothes_total_units = sum(clothes_units)
+meat_total_units = sum(meat_units)
+beverages_total_units = sum(beverages_units)
+office_supplies_total_units = sum(office_supplies_units)
+cosmetics_total_units = sum(cosmetics_units)
+snacks_total_units = sum(snacks_units)
+personal_care_total_units = sum(personal_care_units)
+household_total_units = sum(household_units)
+baby_food_total_units = sum(baby_food_units)
+vegetables_total_units = sum(vegetables_units)
+cereal_total_units = sum(cereal_units)
+
+
+list_of_c_sums = [fruits_sum, clothes_sum, meat_sum, beverages_sum, office_supplies_sum, cosmetics_sum,
+                  snacks_sum, personal_care_sum, household_sum, baby_food_sum, vegetables_sum, cereal_sum]
+list_of_c_types = ["Fruits", "Clothes", "Meat", "Beverages", "Office Supplies", "Cosmetics",
+                   "Snacks", "Personal Care", "Household", "Baby Food", "Vegetables", "Cereal"]
+index = list_of_c_sums.index(max(list_of_c_sums))
+print("The top category is %s." % list_of_c_types[index])
+
+
+list_of_r_sums = [sub_saharan_africa_sum, mideast_and_north_africa_sum, oceania_sum,
+                  europe_sum, asia_sum, c_america_and_caribbean_sum, n_america_sum]
+list_of_r_types = ["Sub-Saharan Africa", "Middle East and North Africa", "Australia and Oceania",
+                   "Europe", "Asia", "Central America and the Caribbean", "North America"]
+index = list_of_r_sums.index(max(list_of_r_sums))
+print("The top region is %s." % list_of_r_types[index])
+
+
+# TO DO
+
 print("The total profit for the Fruits category is %f" % fruits_sum)
 print("The total profit for the Clothes category is %f" % clothes_sum)
 print("The total profit for the Meat category is %f" % meat_sum)
@@ -139,17 +197,15 @@ print("The total profit for the Asia region is %f" % asia_sum)
 print("The total profit for the Central America and the Caribbean region is %f" % c_america_and_caribbean_sum)
 print("The total profit for the North America region is %f" % n_america_sum)
 print()
-
-list_of_c_sums = [fruits_sum, clothes_sum, meat_sum, beverages_sum, office_supplies_sum, cosmetics_sum,
-                  snacks_sum, personal_care_sum, household_sum, baby_food_sum, vegetables_sum, cereal_sum]
-list_of_c_types = ["Fruits", "Clothes", "Meat", "Beverages", "Office Supplies", "Cosmetics",
-                   "Snacks", "Personal Care", "Household", "Baby Food", "Vegetables", "Cereal"]
-index = list_of_c_sums.index(max(list_of_c_sums))
-print("The top category is %s." % list_of_c_types[index])
-
-list_of_r_sums = [sub_saharan_africa_sum, mideast_and_north_africa_sum, oceania_sum,
-                  europe_sum, asia_sum, c_america_and_caribbean_sum, n_america_sum]
-list_of_r_types = ["Sub-Saharan Africa", "Middle East and North Africa", "Australia and Oceania",
-                   "Europe", "Asia", "Central America and the Caribbean", "North America"]
-index = list_of_r_sums.index(max(list_of_r_sums))
-print("The top region is %s." % list_of_r_types[index])
+# print("The average profit per unit for the Fruits category is %f" % )
+# print("The average profit per unit for the Clothes category is %f" % )
+# print("The average profit per unit for the Meat category is %f" % )
+# print("The average profit per unit for the Beverages category is %f" % )
+# print("The average profit per unit for the Office Supplies category is %f" % )
+# print("The average profit per unit for the Cosmetics category is %f" % )
+# print("The average profit per unit for the Snacks category is %f" % )
+# print("The average profit per unit for the Personal Care category is %f" % )
+# print("The average profit per unit for the Baby Food category is %f" % )
+# print("The average profit per unit for the Vegetables category is %f" % )
+# print("The average profit per unit for the Cereal category is %f" % )
+print()
